b976c235a829785c06b9bfa3c62e1b9e0ea7ef7b
Renata Lemos <rllemos04@gmail.com>
2022-02-15 15:15:36 -0500

    geojson

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -45,7 +45,7 @@ L.geoJSON(sanFranAirport, {
     // We turn each feature into a marker on the map.
     onEachFeature: function (feature, layer) {
         console.log(layer);
-        layer.bindPopup("<h2>" + feature.properties.faa + "</h2><hr> <h3>" + feature.properties.city + "," + feature.properties.country.toLocaleString() + "</h3>");
+        layer.bindPopup("<h2>" + feature.properties.name + "</h2><hr> <h3>" + feature.properties.city + "," + feature.properties.country.toLocaleString() + "</h3>");
 
     }
 

7c400c5df7daf9731d76d0a2181838b510023dbc
Renata Lemos <rllemos04@gmail.com>
2022-02-15 14:55:03 -0500

    logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,10 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 // Create the map object with a center and zoom level.
-// Create the map object with center at the San Francisco airport.
-let map = L.map('mapid').setView([37.5, -122.5], 10);
+// // Create the map object with center at the San Francisco airport.
+// let map = L.map('mapid').setView([37.5, -122.5], 10);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // Add GeoJSON data.
 let sanFranAirport =
@@ -84,7 +86,15 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
     maxZoom: 18,
     accessToken: API_KEY
 });
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/rllemos/Mapping_Earthquakes/main/majorAirports.json";
 
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 

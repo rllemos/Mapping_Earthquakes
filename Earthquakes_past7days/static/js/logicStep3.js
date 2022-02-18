b3812ad78c0894913a0a761254d7dd1a6e23a0a3
Renata Lemos <rllemos04@gmail.com>
2022-02-18 13:28:34 -0500

    logic steps 1-5

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -42,6 +42,26 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         }
         return magnitude * 4;
     }
+
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+        return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+        return "#ea822c";
+        }
+        if (magnitude > 3) {
+        return "#ee9c00";
+        }
+        if (magnitude > 2) {
+        return "#eecc00";
+        }
+        if (magnitude > 1) {
+        return "#d4ee00";
+        }
+        return "#98ee00";
+    }
     // This function returns the style data for each of the earthquakes we plot on
     // the map. We pass the magnitude of the earthquake into a function
     // to calculate the radius.
@@ -49,7 +69,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return {
         opacity: 1,
         fillOpacity: 1,
-        fillColor: "#ffae42",
+        fillColor: getColor(feature.properties.mag),
         color: "#000000",
         radius: getRadius(feature.properties.mag),
         stroke: true,
@@ -65,6 +85,11 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 return L.circleMarker(latlng);
             },
           // We set the style for each circleMarker using our styleInfo function.
-            style: styleInfo
+            style: styleInfo,
+            // We create a popup for each circleMarker to display the magnitude and
+            //  location of the earthquake after the marker has been created and styled.
+            onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+          }
         }).addTo(map);
 });
--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -15,22 +15,31 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+  })
+
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
   "Satellite": satelliteStreets
 };
 
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-  center: [39.5, -98.5],
-  zoom: 3,
-  layers: [streets]
-})
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+  };
 
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
@@ -42,6 +51,26 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         }
         return magnitude * 4;
     }
+
+    // This function determines the color of the circle based on the magnitude of the earthquake.
+    function getColor(magnitude) {
+        if (magnitude > 5) {
+        return "#ea2c2c";
+        }
+        if (magnitude > 4) {
+        return "#ea822c";
+        }
+        if (magnitude > 3) {
+        return "#ee9c00";
+        }
+        if (magnitude > 2) {
+        return "#eecc00";
+        }
+        if (magnitude > 1) {
+        return "#d4ee00";
+        }
+        return "#98ee00";
+    }
     // This function returns the style data for each of the earthquakes we plot on
     // the map. We pass the magnitude of the earthquake into a function
     // to calculate the radius.
@@ -49,7 +78,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         return {
         opacity: 1,
         fillOpacity: 1,
-        fillColor: "#ffae42",
+        fillColor: getColor(feature.properties.mag),
         color: "#000000",
         radius: getRadius(feature.properties.mag),
         stroke: true,
@@ -64,7 +93,14 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
                 console.log(data);
                 return L.circleMarker(latlng);
             },
-          // We set the style for each circleMarker using our styleInfo function.
-            style: styleInfo
-        }).addTo(map);
+        // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo,
+        // We create a popup for each circleMarker to display the magnitude and
+        //  location of the earthquake after the marker has been created and styled.
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+          }
+    }).addTo(earthquakes);
+    //Then add earthquake layer to map
+    earthquakes.addTo(map);
 });
